669511b6c92cdf4f9b5968455b0f5d1fb61a91ef
Zin-Wai-Aung <zinwaiaung60@gmail.com>
2023-02-13 22:30:54 +0630

    Project Setup & Adding Global CSS

---
+++ src/views/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="welcome d-flex justify-content-center mt-5 rounded shadow bg-light w-75 m-auto">
+    <h3 class="text-secondary">Welcome</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
