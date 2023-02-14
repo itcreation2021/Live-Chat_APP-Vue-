06afba9a04ed192e31df06aacf67292c9dcad1b3
Zin-Wai-Aung <zinwaiaung60@gmail.com>
2023-02-14 22:37:24 +0630

    Event emmit in setup funciton(enter to chatroom)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -19,7 +19,7 @@ import { ref } from '@vue/reactivity'
 import useLogin from '../composables/useLogin';
 
 export default {
-    setup() {
+    setup(props,context) {
         let email = ref("");
         let password = ref("");
 
@@ -28,7 +28,7 @@ export default {
         let login = async () => {
             let res = await signIn(email.value, password.value)
             if (res) {
-                console.log(res.user)
+                context.emit("enterChatroom")
             }
         }
         return {email, password, login,error }
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="welcome justify-content-center rounded-4 mt-5 shadow bg-light w-75 m-auto">
     <div v-if="showLoginForm">
-      <Login></Login>
+      <Login @enterChatroom="enterChatroom"></Login>
       <p class="text-center pb-3">Not a member? <span @click="showLoginForm = !showLoginForm">Creat Accout!</span></p>
     </div>  
     <div v-else>
-      <Signup></Signup>
+      <Signup @enterChatroom="enterChatroom"></Signup>
       <p class="text-center pb-3">Already member? <span @click="showLoginForm = !showLoginForm">Login Form!</span></p>
     </div>
   </div>
@@ -15,15 +15,21 @@
 import { ref } from '@vue/reactivity'
 import Login from '../components/Login'
 import Signup from '../components/Signup'
+import { useRouter } from 'vue-router'
+
 export default {
   components: {
     Login, Signup
   },
   setup() {
-    let showLoginForm = ref(true)
+    let showLoginForm = ref(true);
+    let router = useRouter();
 
+    let enterChatroom = () => {
+      router.push({name:'Chatroom'})
+    }
 
-    return {showLoginForm}
+    return {showLoginForm,enterChatroom}
     }
 
 }
