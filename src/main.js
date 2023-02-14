c45476989167fc9c3c820f2b26da38e3f8ca964a
Zin-Wai-Aung <zinwaiaung60@gmail.com>
2023-02-15 00:17:19 +0630

    Route Guard & Wait Vue App for our Auth System

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/global.css"
-createApp(App).use(router).mount("#app");
+import {auth} from './firebase/config';
+let app;
+auth.onAuthStateChanged(() => {//connect firebase auth library
+    if (!app) { // when app is empty = once refresh
+         app =createApp(App).use(router).mount("#app");
+    }
+})
